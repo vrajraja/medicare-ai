1ab239f2f3d2bd5d4f53551ef27742fc49527550
Vraj raja <vrajraja588@gmail.com>
2026-04-14 17:40:09 +0530

    fix api route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -319,7 +319,7 @@ controllerRef.current=new AbortController();
 
 try{
 console.log("KEY:", import.meta.env.VITE_API_KEY);
-const response=await fetch("https://medicare-ai-2pa2.onrender.com",{
+const response=await fetch("https://medicare-ai-2pa2.onrender.com/ask",{
 method:"POST",
 headers:{
   "Content-Type":"application/json",
