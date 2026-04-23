730b85a20364c714a86d533aa96b289dd1b436c5
Vraj raja <vrajraja588@gmail.com>
2026-04-23 12:06:07 +0530

    fix cors

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://medicare-ai-jgzz.vercel.app"
-        "https://*.vercel.app"
+    
     ],
     allow_credentials=True,
     allow_methods=["*"],
