f4182985e897e3a0e74d2485728f9a85981d09bf
Vraj raja <vrajraja588@gmail.com>
2026-04-23 12:23:07 +0530

    fix cors

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://medicare-ai-jgzz.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
